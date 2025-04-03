2bcfca5771c69fa456808ae61f997051bfd80411
Dongfu Jiang <dongfu.jdf@gmail.com>
2025-04-03 02:41:13 +0000

    init

---
+++ download_math_hard.sh
@@ -0,0 +1,4 @@
+mkdir -p data/math_hard
+cd data/math_hard
+wget https://huggingface.co/datasets/hkust-nlp/SimpleRL-Zoo-Data/resolve/main/simplelr_qwen_level3to5/train.parquet
+wget https://huggingface.co/datasets/hkust-nlp/SimpleRL-Zoo-Data/resolve/main/simplelr_qwen_level3to5/test.parquet
